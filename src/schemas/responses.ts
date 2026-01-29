abdaff4f199862d19d9677e51b6e4b0af42d2cbf
Edis Hurtic <edis.hurtic@student.hogent.be>
2026-01-29 17:16:53 +0100

    Adding the backend folders

---
+++ src/schemas/responses.ts
@@ -0,0 +1,15 @@
+export const BadRequest400 = {
+  description: "You provided invalid data",
+};
+
+export const Unauthorized401 = {
+  description: "You don't have the permissions to access this resource",
+};
+
+export const NotFound404 = {
+  description: "The data you where looking for doesn't exist",
+};
+
+export const Conflict409 = {
+  description: "The object you where trying to create already exists.",
+};
