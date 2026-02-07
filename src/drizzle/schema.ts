d9f9116a2a792b422198beb8c3e75b60b7b0cc71
Edis Hurtic <edis.hurtic@student.hogent.be>
2026-02-07 17:48:10 +0100

    start of schema

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,7 +1,13 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgEnum, pgTable, varchar } from "drizzle-orm/pg-core";
+
+const genderTypes = pgEnum("genderTypes", ["Male", "Female", "X"]);
 
 export const membersTable = pgTable("members", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   firstname: varchar({length: 50}).notNull(),
   lastname: varchar({length: 50}).notNull(),
+  gender: genderTypes(),
+  email: varchar({length: 255}).notNull().unique(),
+
 })
+
