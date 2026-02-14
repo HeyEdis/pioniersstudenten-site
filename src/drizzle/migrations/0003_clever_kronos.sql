56dae70b923205e0e45609d382b87d1f1c999199
Edis Hurtic <edis.hurtic@student.hogent.be>
2026-02-14 20:18:53 +0100

    Migration to delete registrations when event gets deleted.

---
+++ src/drizzle/migrations/0003_clever_kronos.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "registrations" DROP CONSTRAINT "registrations_event_id_events_id_fk";
+--> statement-breakpoint
+ALTER TABLE "registrations" ADD CONSTRAINT "registrations_event_id_events_id_fk" FOREIGN KEY ("event_id") REFERENCES "public"."events"("id") ON DELETE cascade ON UPDATE no action;
