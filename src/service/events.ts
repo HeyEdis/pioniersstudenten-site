7f2c21b48df8911dd14bf9c414de769a052b5e9b
Edis Hurtic <edis.hurtic@student.hogent.be>
2026-02-14 12:33:32 +0100

    validation fix

--- src/service/events.ts
+++ src/service/events.ts
@@ -6,13 +6,13 @@ import { eq } from "drizzle-orm";
 import ServiceError from "@/core/serviceError";
 
 export const create = async(params: typeof event.$inferInsert) :  Promise<Event> => {
-    if(!params){throw ServiceError.notFound("Alle velden zijn leeg.")}
-    if(!params.label){throw new Error("Label is leeg.")}
-    if(!params.title){throw new Error("Titel is leeg.")}
-    if(!params.date){throw new Error("Datum is leeg.")}
-    if(!params.start_time){throw new Error("Starttijd is leeg.")}
-    if(!params.end_time){throw new Error("Eindtijd is leeg.")}
-    if(!params.description){throw new Error("Omschrijving is leeg.")}
+    if(!params){throw ServiceError.notFound("Alle velden zijn leeg.")};
+    if(!params.label){throw ServiceError.validationFailed("Label is leeg.")};
+    if(!params.title){throw ServiceError.validationFailed("Titel is leeg.")};
+    if(!params.date){throw ServiceError.validationFailed("Datum is leeg.")};
+    if(!params.start_time){throw ServiceError.validationFailed("Starttijd is leeg.")};
+    if(!params.end_time){throw ServiceError.validationFailed("Eindtijd is leeg.")};
+    if(!params.description){throw ServiceError.validationFailed("Omschrijving is leeg.")};
 
     try{
         const [created] = await db
@@ -29,36 +29,57 @@ export const getAll = async() : Promise<Event[]> => {
     return await db.select().from(event);
 };
 
-export const getByLabel = async(label: PioneerLabel) : Promise<Event[]> => {
-    if(!label){throw ServiceError.notFound("Het label heeft geen waarde.")}
+export const getById = async(eventId: number) : Promise<Event> => {
+    if(!eventId){throw ServiceError.validationFailed("Id van het event is niet meegegeven.")};
 
-    return await db.select()
-    .from(event)
-    .where(eq(event.label, label));
+    const [eventById] = await db
+        .select()
+        .from(event)
+        .where(eq(event.id,eventId));
+
+    if(!eventById){
+        throw ServiceError.notFound(`Evenement met id ${eventId} is niet gevonden.`)
+    };
+
+    return eventById;
+
+
 };
 
-export const updateById = async(eventId : number, params: typeof event.$inferInsert) : Promise<Event> => {
-    if (!eventId){throw ServiceError.notFound("Er is geen id van dit event.")}
+export const getByLabel = async(label: PioneerLabel) : Promise<Event[]> => {
+    if(!label){throw ServiceError.notFound("Het label heeft geen waarde.")};
 
-    if(!params){throw ServiceError.notFound("Alle velden zijn leeg.")}
-    if(!params.label){throw new Error("Label is leeg.")}
-    if(!params.title){throw new Error("Titel is leeg.")}
-    if(!params.date){throw new Error("Datum is leeg.")}
-    if(!params.start_time){throw new Error("Starttijd is leeg.")}
-    if(!params.end_time){throw new Error("Eindtijd is leeg.")}
-    if(!params.description){throw new Error("Omschrijving is leeg.")}
+    const eventByLabel = await db
+        .select()
+        .from(event)
+        .where(eq(event.label,label));
+
+    if(eventByLabel.length === 0){
+        throw ServiceError.notFound(`Geen evenementen met het label "${label}" gevonden.`)
+    };
+
+    return eventByLabel;
+};
+
+export const updateById = async(eventId : number, params: Partial<typeof event.$inferInsert>) : Promise<Event> => {
+    if (!eventId){throw ServiceError.notFound("Er is geen id van dit event.")};
+
+    if(!params){throw ServiceError.notFound("Alle velden zijn leeg.")};
+    if(!params.label){throw ServiceError.validationFailed("Label is leeg.")};
+    if(!params.title){throw ServiceError.validationFailed("Titel is leeg.")};
+    if(!params.date){throw ServiceError.validationFailed("Datum is leeg.")};
+    if(!params.start_time){throw ServiceError.validationFailed("Starttijd is leeg.")};
+    if(!params.end_time){throw ServiceError.validationFailed("Eindtijd is leeg.")};
+    if(!params.description){throw ServiceError.validationFailed("Omschrijving is leeg.")};
     
     try {
-        const [updatedEvent] =  await db.update(event)
+        const [updatedEvent] =  await db
+            .update(event)
             .set({...params, 
                 updated_at : new Date(),}
             )
             .where(eq(event.id, eventId))
             .returning();
-
-        if (!updatedEvent) {
-            throw ServiceError.notFound('Dit evenement bestaat niet.');
-        }
         return updatedEvent;
     }catch(error){
         throw handleDBError(error);
@@ -66,7 +87,7 @@ export const updateById = async(eventId : number, params: typeof event.$inferIns
 };
 
 export const deleteById = async(eventId: number) : Promise<void> => {
-    if (!eventId){throw ServiceError.notFound("Er is geen id van dit event.")}
+    if (!eventId){throw ServiceError.notFound("Er is geen id van dit event.")};
 
     try{
         await db.delete(event).where(eq(event.id, eventId));
