1d2a27feca54e4f0e6ce65944d50ade0592fbf88
Edis <edis.hurtic@student.hogent.be>
2026-02-25 17:46:22 +0100

    changing about page to original page

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -1,19 +1,7 @@
-'use client'
-
-import { authClient } from "@/lib/auth-client"
-
-export default function Evenementen() {
-  const email = "admin@example.com"
-  const password = "password123"
-
-  async function signIn() {
-    await authClient.signIn.email({
-      email: email,
-      password: password
-    })
-  }
-
-  console.log("authClient log:" + JSON.stringify(authClient.useSession()));
-
-  return <button onClick={signIn}>sign in</button>
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+    </div>
+  );
 }
