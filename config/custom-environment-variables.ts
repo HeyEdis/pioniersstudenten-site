7dcee35090d517c7fe1c4cbcb87f0a86414c0461
EliasVanMeerssche <elias.vanmeerssche@student.hogent.be>
2026-02-25 15:27:43 +0100

    Add logging and signup configuration to Docker and environment files

--- config/custom-environment-variables.ts
+++ config/custom-environment-variables.ts
@@ -1,3 +1,16 @@
 export default {
-  env: "APP_ENV"
+  env: "APP_ENV",
+  log: {
+    level: "LOG_LEVEL",
+    disabled: {
+      __name: "LOG_DISABLED",
+      __format: "json",
+    },
+  },
+  betterauth: {
+    signupdisabled: {
+      __name: "BETTER_AUTH_SIGNUP_DISABLED",
+      __format: "json",
+    },
+  },
 };
--- config/default.ts
+++ config/default.ts
@@ -1,10 +1,2 @@
 export default {
-  env: process.env.APP_ENV || "development",
-  log: {
-    level: "silly",
-    disabled: false,
-  },
-  betterauth: {
-    signupdisabled: false
-  }
 };
