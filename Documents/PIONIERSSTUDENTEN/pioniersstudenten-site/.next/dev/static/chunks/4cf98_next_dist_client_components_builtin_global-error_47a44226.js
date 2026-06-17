79242bb83a14aad4959d3ad7653cbda2991990bf
Edis <edis.hurtic@student.hogent.be>
2026-06-17 18:28:04 +0200

    chore(): deleting Documents folder

--- Documents/PIONIERSSTUDENTEN/pioniersstudenten-site/.next/dev/static/chunks/4cf98_next_dist_client_components_builtin_global-error_47a44226.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/4cf98_next_dist_92fd5c71._.js"
-],
-    source: "dynamic"
-});
